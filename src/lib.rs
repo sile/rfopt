1cfc7b8a44c533912bd73fabdbd7db3a48e1f55b
Takeru Ohta <phjgt308@gmail.com>
2020-09-17 10:11:44 +0900

    [WIP] Implement ask method

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,35 @@
 use kurobako_core::problem::ProblemSpec;
+use kurobako_core::rng::ArcRng;
 use kurobako_core::solver::Solver;
-use kurobako_core::trial::{EvaluatedTrial, IdGen, NextTrial};
+use kurobako_core::trial::{EvaluatedTrial, IdGen, NextTrial, Params, TrialId};
 use ordered_float::OrderedFloat;
+use rand::distributions::Distribution as _;
 use randomforest::criterion::Mse;
 use randomforest::table::{ColumnType, TableBuilder};
 use randomforest::RandomForestRegressor;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct RfOpt {
-    seed: u64,
     problem: ProblemSpec,
     trials: Vec<Trial>,
+    evaluating: HashMap<TrialId, Params>,
+    rng: ArcRng,
+    // TODO: Phase
 }
 
 impl RfOpt {
     pub fn new(seed: u64, problem: ProblemSpec) -> Self {
         Self {
-            seed,
             problem,
             trials: Vec::new(),
+            evaluating: HashMap::new(),
+            rng: ArcRng::new(seed),
         }
     }
 
     fn fit_rf(&mut self) -> anyhow::Result<RandomForestRegressor> {
+        // TODO: cap outliners
         let mut table = TableBuilder::new();
         let columns = self
             .problem
@@ -50,16 +57,72 @@ impl RfOpt {
 
         Ok(RandomForestRegressor::fit(Mse, table.build()?))
     }
+
+    fn calc_params_ranking(&self) -> anyhow::Result<Vec<usize>> {
+        let mut features = vec![Vec::new(); self.problem.params_domain.variables().len()];
+        let mut target = Vec::new();
+        for t in &self.trials {
+            for (f, &p) in features.iter_mut().zip(t.params.iter()) {
+                f.push(p);
+            }
+            target.push(t.value);
+        }
+
+        let mut fanova =
+            fanova::Fanova::fit(features.iter().map(|f| f.as_slice()).collect(), &target)?;
+        let mut importances = (0..self.problem.params_domain.variables().len())
+            .map(|i| (i, fanova.quantify_importance(&[i])))
+            .collect::<Vec<_>>();
+        importances.sort_by_key(|x| OrderedFloat(x.1.mean));
+        importances.reverse();
+        Ok(importances.into_iter().map(|x| x.0).collect::<Vec<_>>())
+    }
+
+    fn ask_random(&mut self) -> Params {
+        let mut params = Vec::new();
+        for p in self.problem.params_domain.variables() {
+            let param = p.sample(&mut self.rng);
+            params.push(param);
+        }
+        Params::new(params)
+    }
+
+    fn sample(&mut self, params: &[f64], param_index: usize) -> f64 {
+        todo!()
+    }
 }
 
 impl Solver for RfOpt {
     fn ask(&mut self, idg: &mut IdGen) -> kurobako_core::Result<NextTrial> {
-        let rf = self.fit_rf();
-        todo!()
+        let id = idg.generate();
+
+        let params = if self.trials.len() < 10 {
+            self.ask_random()
+        } else {
+            let rf = self.fit_rf().expect("TODO");
+            let ranking = self.calc_params_ranking().expect("TODO");
+            let mut params = vec![std::f64::NAN; self.problem.params_domain.variables().len()];
+            for i in ranking {
+                params[i] = self.sample(&params, i);
+            }
+            Params::new(params)
+        };
+
+        self.evaluating.insert(id, params.clone());
+        Ok(NextTrial {
+            id,
+            params,
+            next_step: Some(self.problem.steps.last()),
+        })
     }
 
     fn tell(&mut self, trial: EvaluatedTrial) -> kurobako_core::Result<()> {
-        todo!()
+        let params = self.evaluating.remove(&trial.id).expect("TODO");
+        self.trials.push(Trial {
+            params: params.into_vec(),
+            value: trial.values[0],
+        });
+        Ok(())
     }
 }
 
